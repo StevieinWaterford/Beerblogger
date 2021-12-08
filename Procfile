17bdf0f3f5f1dd4704c776085463b6dd59db67df
StevieinWaterford <stevieindub@gmail.com>
2021-12-08 21:53:32 +0000

    Add deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn belgianbeers.wsgi
---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ["DATABASE_URL"] = "postgres://itxqahcavierxr:aa7da979628a842c77b1348fe78c5e3e50d5ca61f4679d37d69716c9bda0654c@ec2-52-19-164-214.eu-west-1.compute.amazonaws.com:5432/d1s8uqq7cdkfnm"
+os.environ["SECRET_KEY"] = "EncodedSecretKey2222!!"
+os.environ[ "CLOUDINARY_URL"] = "cloudinary://884173494267683:2A1aK79n30uxfpSjxtgcjAXpt_g@dzbad1rgb"
